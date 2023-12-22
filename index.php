2276e156a6ffdf2df3536e6bd5bda58e166e5cc1
rafi andrea <rafiandrealesmana@gmail.com>
2023-12-22 17:22:31 +0700

    sepuhhh bantu akuh

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+require("./module/function.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./src/css/main.css" />
+    <!-- JS -->
+    <script src="./src/js/app.js" defer></script>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Disorganized</title>
+</head>
+
+<body>
+    <div id="root">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Article</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Profile
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="http://localhost/coba2/views/user/login">Login</a></li>
+                                <li><a class="dropdown-item" href="http://localhost/coba2/views/user/register">Register</a></li>
+                                <li><a class="dropdown-item" href="http://localhost/coba2/views/dashboard/">Dashboard</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Carousel -->
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://source.unsplash.com/1200x250?natural" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://source.unsplash.com/1200x250?sky" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://source.unsplash.com/1200x250?ocean" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!-- Table -->
+        <div class="container text-center">
+            <div class="row p-5">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/1200x250?natural" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/1200x250?sky" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/1200x250?ocean" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ module/function.php
@@ -0,0 +1,78 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "artikel");
+
+if (!$connect) {
+    die("Koneksi error" . mysqli_connect_error());
+}
+
+function generateId($length = 9)
+{
+    $chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+    $lengChars = strlen($chars);
+    $output = "";
+    for ($i = 0; $i < $length; $i++) {
+        $output .= $chars[rand(0, $lengChars - 1)];
+    }
+    return $output;
+}
+
+function getData($sql)
+{
+    global $connect;
+    $data = [];
+    $result = mysqli_query($connect, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+        $data[] = $row;
+    }
+    return $data;
+}
+
+function register()
+{
+    global $connect;
+    $id = generateId(9);
+    $username = stripslashes($_POST["name"]);
+    $email = stripslashes($_POST["email"]);
+    $address = stripslashes($_POST["address"]);
+    $phone = $_POST["phone"];
+    $password = mysqli_real_escape_string($connect, $_POST["password"]);
+    $confirmPassword = mysqli_real_escape_string($connect, $_POST["confirm-password"]);
+
+    $emailDuplicated = getData("SELECT * FROM users WHERE email = '$email'")[0]["email"];
+    $phoneDuplicated = getData("SELECT * FROM users WHERE phone = '$phone'")[0]["phone"];
+
+    if ($password !== $confirmPassword) {
+        echo "
+        <script>
+            alert('Password tidak sesuai');
+            document.location.href = 'http://localhost/crud/views/user/register/';
+        </script>
+        ";
+        return false;
+    }
+
+    if (strtolower($email) === strtolower($emailDuplicated)) {
+        echo "
+        <script>
+            alert('Email sudah terdaftar.');
+            document.location.href = 'http://localhost/crud/views/user/register/';
+        </script>
+        ";
+        return false;
+    }
+    if ($phone === $phoneDuplicated) {
+        echo "
+        <script>
+            alert('Nomor HP sudah terdaftar.');
+            document.location.href = 'http://localhost/crud/views/user/register/';
+        </script>
+        ";
+        return false;
+    }
+
+    $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (id,name,email,phone,address,password) VALUES ('$id','$username','$email','$phone','$address','$passwordHashed')";
+
+    mysqli_query($connect, $sql);
+    return mysqli_affected_rows($connect);
+}
